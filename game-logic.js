f2e968fbb0c383ef018b0edee755ed5c5ca381c3
Cj Alexander <clauvinsalexander@gmail.com>
2023-11-05 20:18:02 -0500

    Added buttons to capture user input with event delegation

--- game-logic.js
+++ game-logic.js
@@ -1,50 +1,56 @@
 getComputerChoice = () => {
-    const choices = ['rock', 'paper', 'scissors']
-    let computerChoice = choices[(Math.floor(Math.random() * 3))]
-    return computerChoice
+  const choices = ["rock", "paper", "scissors"];
+  let computerChoice = choices[Math.floor(Math.random() * 3)];
+  return computerChoice;
 };
 
 getPlayerSelection = () => {
-    getInput = prompt('Pick your move')
-    return getInput.toLowerCase()
+  getInput = prompt("Pick your move");
+  return getInput.toLowerCase();
 };
 
 playRound = (playerSelection, computerChoice) => {
-    if (playerSelection === computerChoice) {
-        return "Tie!"
-    } else if (playerSelection === 'rock' && computerChoice === 'scissors') {
-        return "Player wins!"
-    } else if (playerSelection === 'rock' && computerChoice === 'paper') {
-        return "Computer wins!"
-    } else if (playerSelection === 'scissors' && computerChoice === 'rock') {
-        return "Computer wins!"
-    } else if (playerSelection === 'scissors' && computerChoice === 'paper') {
-        return "Player wins!"
-    } else if (playerSelection === 'paper' && computerChoice === 'scissors') {
-        return "Computer wins!"
-    } else if (playerSelection === 'paper' && computerChoice === 'rock') {
-        return "Player wins!"
-    }
+  if (playerSelection === computerChoice) {
+    return "Tie!";
+  } else if (playerSelection === "rock" && computerChoice === "scissors") {
+    return "Player wins!";
+  } else if (playerSelection === "rock" && computerChoice === "paper") {
+    return "Computer wins!";
+  } else if (playerSelection === "scissors" && computerChoice === "rock") {
+    return "Computer wins!";
+  } else if (playerSelection === "scissors" && computerChoice === "paper") {
+    return "Player wins!";
+  } else if (playerSelection === "paper" && computerChoice === "scissors") {
+    return "Computer wins!";
+  } else if (playerSelection === "paper" && computerChoice === "rock") {
+    return "Player wins!";
+  }
 };
 
 function game() {
-    let playerWins = 0
-    let computerWins = 0
-    let ties = 0
-    for (let rounds = 0; rounds !== 5; rounds++) {
-        let player = getPlayerSelection()
-        let computer = getComputerChoice()
-        let result = playRound(player, computer)
-        if (result.startsWith("Player")) {
-            playerWins ++
-        } else if (result.startsWith('Computer')) {
-            computerWins ++
-        } else {
-            ties ++
-        }
-        console.log(result)
-    }
-    console.log(`Wins: ${playerWins}, Losses: ${computerWins}, Ties: ${ties}`)
+  let playerWins = 0;
+  let computerWins = 0;
+  let ties = 0;
+
+  console.log(`Wins: ${playerWins}, Losses: ${computerWins}, Ties: ${ties}`);
 }
 
-game()
+// Used event delegation to lower the amount of listeners i'd have to use
+
+const userChoice = document.querySelector(".select-buttons");
+
+userChoice.addEventListener("click", (e) => {
+  let target = e.target;
+
+  switch (target.id) {
+    case "rock":
+      console.log("ROCK");
+      break;
+    case "scissors":
+      console.log("SCISSORS");
+      break;
+    case "paper":
+      console.log("PAPER");
+      break;
+  }
+});
